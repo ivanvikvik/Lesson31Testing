7c082d746982b0245b77ce08d61a767b9633ea14
ivanvikvik <ivanvikvik@gmail.com>
2023-05-18 21:50:48 +0300

    Use special methods setUp and tearDown

--- task03/calculator_test.py
+++ task03/calculator_test.py
@@ -4,36 +4,41 @@ from calculator import Calculator
 
 class CalculatorTest(unittest.TestCase):
 
+    def setUp(self):
+        # print("***** setUp *****")
+        self.calc = Calculator()
+
+    def tearDown(self):
+        # print("***** tearDown *****")
+        del self.calc
+
     def testAddPositive(self):
         # print("testAddPositive")
         a = 5
         b = 6
         expected = 11
 
-        calc = Calculator()
-        actual = calc.add(a, b)
+        actual = self.calc.add(a, b)
 
         self.assertEqual(expected, actual)
 
     def testSubPositive(self):
-        print("testSubPositive")
+        # print("testSubPositive")
         a = 5
         b = 6
         expected = -1
 
-        calc = Calculator()
-        actual = calc.sub(a, b)
+        actual = self.calc.sub(a, b)
 
         self.assertEqual(expected, actual)
 
     def testMulPositive(self):
-        print("testMulPositive")
+        # print("testMulPositive")
         a = 5
         b = 6
         expected = 30
 
-        calc = Calculator()
-        actual = calc.mul(a, b)
+        actual = self.calc.mul(a, b)
 
         self.assertEqual(expected, actual)
 
@@ -43,8 +48,7 @@ class CalculatorTest(unittest.TestCase):
         b = 2
         expected = 5
 
-        calc = Calculator()
-        actual = calc.div(a, b)
+        actual = self.calc.div(a, b)
 
         self.assertEqual(expected, actual)
 
